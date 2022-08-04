2598a9ac4bc46634a09a52b23806226bcae80734
Maksim <maksimfilimonchuk@gmail.com>
2022-08-04 17:04:27 +0300

    added kb conf

---
+++ keyboards/discipline/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#include "config_common.h"
+#define VIAL_KEYBOARD_UID {0xE0, 0x4A, 0xE8, 0x93, 0x1C, 0x0F, 0xE1, 0x44}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/fusion/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#include "config_common.h"
+#define VIAL_KEYBOARD_UID {0xCB, 0x87, 0xC3, 0xB4, 0xCA, 0xEB, 0xA3, 0x2A}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
