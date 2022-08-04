5e3140079c3d2ea27daec69e8d0c0e760bdec61a
Maksim <maksimfilimonchuk@gmail.com>
2022-08-04 14:58:28 +0300

    Updated, optimized and tested

--- keyboards/fusion/config.h
+++ keyboards/fusion/config.h
@@ -1,32 +1,17 @@
-/*Copyright 2019 coseyfannitutti
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #pragma once
 
 #include "config_common.h"
 
 #define VENDOR_ID       0x6B62
-#define PRODUCT_ID      0x6869
+#define PRODUCT_ID      0x4655
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    SciencePirate
-#define PRODUCT         DISCIPLINE
+#define PRODUCT         FUSION
 
 /* key matrix size */
 #define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define MATRIX_COLS 4
 
 /*
  * Keyboard Matrix Assignments
@@ -39,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-#define MATRIX_ROW_PINS { B2, A1, B1, A0, B0 }
-#define MATRIX_COL_PINS { A2, B3, A3, B4, A4, D5, D6, C6, C5, C4, C3, C2, C1, C0, D7 }
+#define MATRIX_ROW_PINS { B0, B4, B3, B2, B1 }
+#define MATRIX_COL_PINS { A3, A2, A1, A0}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
@@ -48,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USB_MAX_POWER_CONSUMPTION 100
 
-#define ENCODERS_PAD_A { D1 }
-#define ENCODERS_PAD_B { D0 }
+#define ENCODERS_PAD_A { D0, D5 }
+#define ENCODERS_PAD_B { D1, D6 }
 #define ENCODER_DIRECTION_FLIP
 #define ENCODER_RESOLUTION 4
 #define TAP_CODE_DELAY 10
